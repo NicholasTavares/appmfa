b9df15d7d68502f7497559900259a553e2b34cdd
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-01 15:18:05 -0300

    feat: global colors

---
+++ src/global/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    white: "#fff",
+    "gray/50": "#F9FAFB",
+    "gray/200": "#e5e7eb",
+    "gray/300": "#D1D5DB",
+    "gray/400": "#9CA3AF",
+    "gray/500": "#6B7280",
+    "gray/700": "#404040",
+    "gray/800": "#262626",
+    "gray/900": "#1C1917",
+    "lime/400": "#A3E635",
+    "lime/500": "#84CC16",
+    "lime/600": "#65A30D",
+    "lime/700": "#4d7c0f",
+    "rose/600": "#E11D48",
+  },
+};
+
+export default theme;

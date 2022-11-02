af874039eefb7199c23edb5ca25d75a36875654b
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-02 14:23:47 -0300

    chore: config typescript and jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   testPathIgnorePatterns: ["/node_modules/"],
   collectCoverage: true,
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+  modulePaths: ["<rootDir>/src/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          ["@babel/preset-env", { targets: { esmodules: true } }],
+          ["@babel/preset-react", { runtime: "automatic" }],
+          "@babel/preset-typescript",
+        ],
+      },
+    ],
+  },
 };

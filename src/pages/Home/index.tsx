7d34557c577912d0db06fb6c7e09ba444cd1f8d4
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-05 17:53:57 -0300

    feat: render components and add grid container to tokens components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,32 @@
-import { Navbar } from "../../components/Navbar";
 import * as S from "./styles";
+import { Token } from "../../components/Token";
+import { Navbar } from "../../components/Navbar";
+import { FixedAddButton } from "../../components/FixedAddButton";
 
 export const Home = () => {
   return (
     <S.Container>
       <Navbar />
+      <S.TokensCountContainer>
+        <S.TokensCount>Tokens (5)</S.TokensCount>
+      </S.TokensCountContainer>
+      <S.GridContainer>
+        <Token title="Binance (tiago@email.com)" token="470 864" time="0" />
+        <Token title="Google (tiago@email.com)" token="823 900" time="0" />
+        <Token title="Github (tiago@email.com)" token="115 763" time="0" />
+        <Token title="Coinbase (tiago@email.com)" token="780 199" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+        <Token title="Binance (tiago@email.com)" token="007 555" time="0" />
+      </S.GridContainer>
+      <FixedAddButton />
     </S.Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,40 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div`
+  position: relative;
+  overflow: hidden;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
   padding: 2rem;
+`;
+
+export const TokensCountContainer = styled.div`
+  display: flex;
+  margin-bottom: 3rem;
+`;
+
+export const TokensCount = styled.h2`
+  font-size: 1.7rem;
+`;
+
+export const GridContainer = styled.main`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-rows: repeat(1, 1fr);
+  grid-template-columns: repeat(1, 1fr);
+  margin-bottom: 6rem;
+
+  ${media.greaterThan("small")`
+    grid-template-columns: repeat(2, 1fr);
+  `}
+
+  ${media.greaterThan("medium")`
+    grid-template-columns: repeat(3, 1fr);
+  `}
+
+  ${media.greaterThan("large")`
+    grid-template-columns: repeat(4, 1fr);
+  `}
 `;

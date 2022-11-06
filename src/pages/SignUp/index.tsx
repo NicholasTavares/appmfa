74b989725867ff1920e4e9c8cbcacef763b8bdad
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-06 00:06:51 -0300

    feat: sign up page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { isValidName } from "../../utils/isValidName";
+import { isValidEmail } from "../../utils/isValidEmail";
+import * as S from "./styles";
+
+type SignUpPost = {
+  email: string;
+  name: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const SignUp = () => {
+  const [seePassword, setSeePassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isValid, errors },
+  } = useForm<SignUpPost>({ mode: "onChange" });
+  const onSubmit: SubmitHandler<SignUpPost> = ({
+    email,
+    name,
+    password,
+    password_confirmation,
+  }) => {
+    console.log(email, name, password, password_confirmation);
+  };
+
+  console.log(errors);
+
+  return (
+    <S.Container>
+      <S.FormContainer onSubmit={handleSubmit(onSubmit)}>
+        <S.LogoContainer>
+          <S.Logo />
+          <S.LogoText>Multi Factor Auth</S.LogoText>
+        </S.LogoContainer>
+
+        <S.LabelFieldContainer>
+          <S.Label htmlFor="name">Name</S.Label>
+          <S.FieldContainer>
+            <S.Field
+              type="name"
+              placeholder="Maria"
+              {...register("name", { required: true, validate: isValidName })}
+            />
+          </S.FieldContainer>
+          {errors.name && (
+            <S.ErrorMessage>
+              Name must have at minimum 4 characters
+            </S.ErrorMessage>
+          )}
+        </S.LabelFieldContainer>
+
+        <S.LabelFieldContainer>
+          <S.Label htmlFor="email">Email</S.Label>
+          <S.FieldContainer>
+            <S.Field
+              type="email"
+              placeholder="maria@email.com"
+              {...register("email", { required: true, validate: isValidEmail })}
+            />
+          </S.FieldContainer>
+          {errors.email && <S.ErrorMessage>Invalid email</S.ErrorMessage>}
+        </S.LabelFieldContainer>
+
+        <S.LabelFieldContainer>
+          <S.Label htmlFor="password">Password</S.Label>
+          <S.FieldContainer>
+            <S.Field
+              type={!seePassword ? "password" : "text"}
+              placeholder="password"
+              {...register("password", { required: true, min: 6 })}
+            />
+            <S.IconContainer onClick={() => setSeePassword(!seePassword)}>
+              {!seePassword ? <S.DontSeePasswordIcon /> : <S.SeePasswordIcon />}
+            </S.IconContainer>
+          </S.FieldContainer>
+        </S.LabelFieldContainer>
+
+        <S.LabelFieldContainer>
+          <S.Label htmlFor="password_confirmation">
+            Password confirmation
+          </S.Label>
+          <S.FieldContainer>
+            <S.Field
+              type={!seePassword ? "password" : "text"}
+              placeholder="password"
+              {...register("password_confirmation", {
+                required: true,
+                validate: (val: string) => {
+                  if (watch("password") !== val)
+                    return "Your passwords do not match";
+                },
+              })}
+            />
+          </S.FieldContainer>
+          {errors.password_confirmation && (
+            <S.ErrorMessage>Your passwords do not match</S.ErrorMessage>
+          )}
+        </S.LabelFieldContainer>
+
+        <S.Button disabled={!isValid}>SignIn</S.Button>
+
+        <S.RedirectContainer>
+          <S.Redirect to="/signin">Already have an account?</S.Redirect>
+        </S.RedirectContainer>
+      </S.FormContainer>
+    </S.Container>
+  );
+};
+
+export default SignUp;
---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,146 @@
+import { Vault, EyeSlash, Eye } from "phosphor-react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import media from "styled-media-query";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors["gray/50"]};
+  height: 100vh;
+  max-height: 100vh;
+`;
+
+export const FormContainer = styled.form`
+  transform: translateY(-10%);
+  display: flex;
+  flex-direction: column;
+  border: 1px solid ${({ theme }) => theme.colors["gray/300"]};
+  padding: 1.5rem;
+  border-radius: 2rem;
+  background-color: ${({ theme }) => theme.colors["stone/200"]};
+  width: 28rem;
+
+  ${media.greaterThan("medium")`
+    transform: translateY(0);
+  `}
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+`;
+
+export const Logo = styled(Vault)`
+  font-size: 3rem;
+  margin-right: 1rem;
+  color: ${({ theme }) => theme.colors["gray/800"]};
+`;
+
+export const LogoText = styled.h1`
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors["gray/800"]};
+`;
+
+export const LabelFieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  &:not(last-child) {
+    margin-bottom: 1.5rem;
+  }
+`;
+
+export const Label = styled.label`
+  font-weight: 500;
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.colors["gray/800"]};
+  margin-bottom: 0.25rem;
+`;
+
+export const FieldContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colors["gray/400"]};
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 1rem;
+  padding: 0.75rem 1rem;
+  width: 100%;
+`;
+
+export const Field = styled.input`
+  text-decoration: none;
+  background: none;
+  outline: none;
+  font-size: 1.3rem;
+  width: 90%;
+  border: none;
+`;
+
+export const IconContainer = styled.div`
+  cursor: pointer;
+  width: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SeePasswordIcon = styled(Eye)`
+  font-size: 1.5rem;
+`;
+
+export const DontSeePasswordIcon = styled(EyeSlash)`
+  font-size: 1.5rem;
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+  font-weight: bold;
+  margin-top: 1rem;
+  width: 100%;
+  height: 3rem;
+  background-color: ${({ theme, disabled }) =>
+    !disabled ? theme.colors["lime/500"] : theme.colors["gray/300"]};
+  color: ${({ theme }) => theme.colors["gray/50"]};
+  border: 1px solid ${({ theme }) => theme.colors["gray/300"]};
+  border-radius: 1rem;
+  font-size: 1.3rem;
+  transition: all 500ms;
+
+  &:hover {
+    background-color: ${({ theme, disabled }) =>
+      !disabled && theme.colors["lime/600"]};
+  }
+
+  &:active {
+    background-color: ${({ theme, disabled }) =>
+      !disabled && theme.colors["lime/700"]};
+    transform: scale(1.02);
+  }
+`;
+
+export const RedirectContainer = styled.div`
+  display: flex;
+`;
+
+export const Redirect = styled(Link)`
+  cursor: pointer;
+  text-decoration: none;
+  margin-top: 2rem;
+  font-size: 1.3rem;
+  color: ${({ theme }) => theme.colors["cyan/700"]};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const ErrorMessage = styled.div`
+  margin-top: 0.2rem;
+  font-size: 1.2rem;
+  max-width: 100%;
+  color: ${({ theme }) => theme.colors["gray/700"]};
+`;

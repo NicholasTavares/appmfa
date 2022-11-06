ba150e43b9e843f7fb6880f70d529eb8bf5ff8de
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-05 23:00:37 -0300

    feat: add new signin route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Splash from "./pages/Splash";
 
 const Home = lazy(() => import("./pages/Home"));
+const SignIn = lazy(() => import("./pages/SignIn"));
 
 const App = () => {
   return (
     <Suspense fallback={<Splash />}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Suspense>
   );

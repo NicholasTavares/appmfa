19b1386f8b4aa6963ce11119ebb07481043d9699
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-05 23:01:29 -0300

    feat: signin page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,65 @@
+import { memo, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { isValidEmail } from "../../utils/isValidEmail";
+import * as S from "./styles";
+
+type SignInPost = {
+  email: string;
+  password: string;
+};
+
+const SignIn = () => {
+  const [seePassword, setSeePassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<SignInPost>({ mode: "onChange" });
+  const onSubmit: SubmitHandler<SignInPost> = ({ email, password }) => {
+    console.log(email, password);
+  };
+
+  return (
+    <S.Container>
+      <S.FormContainer onSubmit={handleSubmit(onSubmit)}>
+        <S.LogoContainer>
+          <S.Logo />
+          <S.LogoText>Multi Factor Auth</S.LogoText>
+        </S.LogoContainer>
+
+        <S.LabelFieldContainer>
+          <S.Label htmlFor="email">Email</S.Label>
+          <S.FieldContainer>
+            <S.Field
+              type="email"
+              placeholder="maria@email.com"
+              {...register("email", { required: true, validate: isValidEmail })}
+            />
+          </S.FieldContainer>
+        </S.LabelFieldContainer>
+
+        <S.LabelFieldContainer>
+          <S.Label htmlFor="password">Password</S.Label>
+          <S.FieldContainer>
+            <S.Field
+              type={!seePassword ? "password" : "text"}
+              placeholder="password"
+              {...register("password", { required: true })}
+            />
+            <S.IconContainer onClick={() => setSeePassword(!seePassword)}>
+              {!seePassword ? <S.DontSeePasswordIcon /> : <S.SeePasswordIcon />}
+            </S.IconContainer>
+          </S.FieldContainer>
+        </S.LabelFieldContainer>
+
+        <S.Button disabled={!isValid}>SignIn</S.Button>
+
+        <S.ForgotPasswordContainer>
+          <S.ForgotPassword>Forgot password?</S.ForgotPassword>
+        </S.ForgotPasswordContainer>
+      </S.FormContainer>
+    </S.Container>
+  );
+};
+
+export default memo(SignIn);

78a52a6dc7132d29742bf7d105141e9146ac62f2
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-06 01:01:53 -0300

    feat: config react query and security disable react dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import GlobalStyle from "./global/styles";
 import theme from "./global/theme";
 
+const queryClient = new QueryClient();
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );

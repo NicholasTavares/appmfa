7c031ca93859e49855cce6137126bcfc16e401cb
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-11 14:47:39 -0300

    feat: add private and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import Splash from "./pages/Splash";
 const Home = lazy(() => import("./pages/Home"));
 const SignIn = lazy(() => import("./pages/SignIn"));
 const SignUp = lazy(() => import("./pages/SignUp"));
+const PrivateRoute = lazy(() => import("./components/PrivateRoute"));
+const PublicRoute = lazy(() => import("./components/PublicRoute"));
 
 const App = () => {
   return (
     <Suspense fallback={<Splash />}>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route element={<PublicRoute />}>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
       </Routes>
     </Suspense>
   );
